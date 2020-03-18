68abb0bf006de2e8ee740f9732fcfd5a7b6914b6
Sneha Rawat <sneharawatt@gmail.com>
2020-03-18 15:47:33 +0530

    Add files via upload

---
+++ pdftotextmaybe.py
@@ -0,0 +1,35 @@
+#easy_install pdfquery
+import pdfquery
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+def convert(fname, pages=None):
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
+
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    infile = open(fname, 'rb')
+    for page in PDFPage.get_pages(infile, pagenums):
+        interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    text_file = open("Resume.txt", "w" ,encoding="utf-8")
+    text_file.write(text)
+    output.close
+    text_file.close()
+    return text 
+
+convert("Resume.pdf")
